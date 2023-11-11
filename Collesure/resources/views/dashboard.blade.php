6fcaebed93e9c8db448bc3149609c58d4a2a1581
zumi <2301902@s.asojuku.ac.jp>
2023-11-11 15:48:58 +0900

    Collesure更新

--- Collesure/resources/views/dashboard.blade.php
+++ Collesure/resources/views/dashboard.blade.php
@@ -5,6 +5,12 @@
         </h2>
     </x-slot>
 
+            <div>
+                @auth
+                <h1>{{ Auth::user()->email}}さん、ようこそ！</h1>
+                @endauth
+            </div>
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
